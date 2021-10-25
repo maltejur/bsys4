142e502244aa94eca86514bd793bf86edee76ed1
Bert van der Weerd <bert@stanzabird.nl>
2021-10-25 13:06:54 +0200

    updates

--- Makefile
+++ Makefile
@@ -1,23 +1,66 @@
-.PHONY : all help clean veryclean check update
+.PHONY : all help clean veryclean check update build buildenv source artifacts
 
 version_file=./version
 version:=$(shell cat $(version_file))
 
-all :
-	make -C source all
+all : source buildenv build artifacts
 
 help :
-	@echo "use: make [all] [help] [clean] [veryclean] [check|update]"
+	@echo "use: make [all] [help] [clean] [veryclean] [check|update] [build] [buildenv]"
 	@echo ""
 	@echo "  'update' or 'check' - Check for new Firefox version and "
 	@echo "                        update the './version"
 
 clean :
 	make -C source clean
+	make -C build/debian10 clean
 
-veryclean :
+veryclean : clean
 	make -C source veryclean
+	make -C build/debian10 veryclean
+
+#
+# Keeping ./version up to date.
+#
 
 check : update
 update :
 	@python3 update-version.py
+
+#
+# Making the source tarball
+#
+
+source :
+	make -C source all
+
+#
+# buildenv: Creating build environment and performing the build
+#
+
+buildenv :
+	make -C buildenv/debian10 build
+
+no-cache :
+	make -C buildenv/debian10 no-cache
+
+upload :
+	make -C buildenv/debian10 upload
+
+#
+# build: Building the mozilla tree and artifact
+#
+
+source/librewolf-$(version).source.tar.gz :
+	make -C source all
+
+build : source/librewolf-$(version).source.tar.gz
+	make -C build/debian10 build
+
+#
+# artifacts
+#
+
+artifacts : 
+	make -C artifacts all
+
--- build/debian10/build.py
+++ build/debian10/build.py
@@ -59,20 +59,59 @@ def enter_srcdir(_dir = None):
         except:
             print("fatal error: can't change to '{}' folder.".format(dir))
             script_exit(1)
-        
+
 def leave_srcdir():
     print("cd ..")
     if not options.no_execute:
         os.chdir("..")
 
 
+#
+# The actual script
+#
 
-print('[debug] build/debian10/build.py: Hello, world!')
+#
+# post_build stage:
+#
 
-script = os.path.realpath(__file__)
-print("[debug] Script path:", script)
+
+def get_objdir():
+    pattern = "obj-*"
+    retval = glob.glob(pattern)
+    if options.no_execute:
+        return "obj-XXX"
+    if len(retval) != 1:
+        print("fatal error: in execute_lw_post_build(): cannot glob build output folder '{}'".format(pattern))
+        script_exit(1)
+    return retval[0]
+        
+
+def post_build():
+    print('[debug] --- post_build stage ---------------------------------------')
+    dirname = get_objdir()
+
+    distfolder = "dist/bin"
+    if is_macos:
+        distfolder = 'dist/LibreWolf.app/Contents/Resources'
+
+    if not options.no_execute:
+        os.makedirs("{}/{}/defaults/pref".format(dirname,distfolder), exist_ok=True)
+        os.makedirs("{}/{}/distribution".format(dirname,distfolder), exist_ok=True)
+
+    exec('git clone https://gitlab.com/librewolf-community/settings.git')
+    exec("cp -v settings/defaults/pref/local-settings.js {}/{}/defaults/pref/".format(dirname,distfolder))
+    exec("cp -v settings/distribution/policies.json {}/{}/distribution/".format(dirname,distfolder))
+    exec("cp -v settings/librewolf.cfg {}/{}/".format(dirname,distfolder))
+    exec('rm -rf settings')
+
+
 
 # my build folder will be:
+script = os.path.realpath(__file__)
 build_folder = os.path.dirname(script)
 enter_srcdir(build_folder)
+
+# perform the build
 exec('./mach build')
+post_build()
+exec('./mach package')
