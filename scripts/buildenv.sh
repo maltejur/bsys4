7a187d7a2ab15cebedce6baf975f461b44258073
Bert van der Weerd <bert@stanzabird.nl>
2022-01-03 13:46:56 +0100

    the docker image builds now actually use the scripts/buildenv.sh script

--- scripts/buildenv.sh
+++ scripts/buildenv.sh
@@ -1,6 +1,18 @@
 set -e
+
+# install wasi
+rm -f setup-wasi-linux.sh
+wget "https://gitlab.com/librewolf-community/browser/source/-/raw/main/scripts/setup-wasi-linux.sh"
+chmod +x setup-wasi-linux.sh
+bash setup-wasi-linux.sh
+
+# normal bootstrapping
 cd /mozilla-unified
 ./mach --no-interactive bootstrap --application-choice=browser
+
+# adding some python packages
+pip install testresources pycairo
+
 exit 0
 
 
